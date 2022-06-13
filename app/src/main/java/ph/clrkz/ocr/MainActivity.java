6b344c5d2e85a19bf04e18e1b94c23efbd0984ee
Clarence Andaya <clarenceandaya8@gmail.com>
2022-06-13 16:21:23 +0800

    Initial FIles

---
+++ app/src/main/java/ph/clrkz/ocr/MainActivity.java
@@ -0,0 +1,299 @@
+/*
+ * Copyright (c) 2022 Nicolò Cantori
+ *
+ * This file is part of OCR Camera.
+ *
+ * OCR Camera is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OCR Camera is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCR Camera. If not, see <http ://www.gnu.org/licenses/>.
+ */
+
+package ph.clrkz.ocr;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private ImageView imageView;
+    private File photoFile;
+    Uri image_uri;
+
+    private static final int CAMERA_REQUEST_CODE=200;
+    private static final int STORAGE_REQUEST_CODE=400;
+    private static final int IMAGE_PICK_GALLERY_CODE=1000;
+    private static final int IMAGE_PICK_CAMERA_CODE=1001;
+    private static final int WRITE_EXTERNAL_STORAGE_CODE=1;
+
+    String cameraPermission[];
+    String storagePermission[];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        File directory = new File(getFilesDir(), "photos");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+
+        Button selectPicture = findViewById(R.id.button2);
+        selectPicture.setOnClickListener(v -> onSelectPicture());
+        cameraPermission=new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission=new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("", textView.getText());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(MainActivity.this, "Copied to clipboard", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void onSelectPicture() {
+        String[] items={" Camera"," Gallery"};
+        AlertDialog.Builder dialog= new AlertDialog.Builder(this);
+        dialog.setTitle("Select Image");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if(which==0){
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }
+                    else{
+                        pickCamera();
+                    }
+                }
+                if(which==1){
+                    if(!checkStoragePermission()){
+                        requestStoragePermission();
+                    }
+                    else{
+                        pickGallery();
+                    }
+                }
+            }
+        });
+        dialog.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode==IMAGE_PICK_GALLERY_CODE){
+                CropImage.activity(data.getData()).setGuidelines(CropImageView.Guidelines.ON).start(this);
+            }
+            if(requestCode==IMAGE_PICK_CAMERA_CODE){
+                CropImage.activity(image_uri).setGuidelines(CropImageView.Guidelines.ON).start(this);
+            }
+
+            if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+                CropImage.ActivityResult result=CropImage.getActivityResult(data);
+                if(resultCode== RESULT_OK){
+                    Uri resultUri=result.getUri();
+                    try{
+                        photoFile = getFile(getApplicationContext(),resultUri);
+                    }catch(Exception e){
+                        Log.e("URI to FILE",e.toString());
+                    }
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                    if (bitmap != null) {
+                        /* Rotate the image if necessary: */
+//                        try {
+//                            ExifInterface exif = new ExifInterface(photoFile.getAbsolutePath());
+//                            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+//                            int rotation = 0;
+//                            switch (orientation) {
+//                                case ExifInterface.ORIENTATION_ROTATE_90:
+//                                    rotation = 90;
+//                                    break;
+//                                case ExifInterface.ORIENTATION_ROTATE_180:
+//                                    rotation = 180;
+//                                    break;
+//                                case ExifInterface.ORIENTATION_ROTATE_270:
+//                                    rotation = 270;
+//                                    break;
+//                            }
+//                            if (rotation > 0) {
+//                                Matrix matrix = new Matrix();
+//                                matrix.postRotate(rotation);
+//                                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+//                            }
+//                        } catch (IOException ignored) {}
+
+                        ImageView ivPhoto = findViewById(R.id.imageView);
+                        ivPhoto.setImageBitmap(bitmap);
+
+                        /*
+                         * Text recognition:
+                         */
+
+                        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+                        Frame frameImage = new Frame.Builder().setBitmap(bitmap).build();
+
+                        SparseArray<TextBlock> textBlockSparseArray = textRecognizer.detect(frameImage);
+                        String stringImageText = "";
+                        for (int i = 0; i < textBlockSparseArray.size(); i++)
+                        {
+                            TextBlock textBlock = textBlockSparseArray.get(textBlockSparseArray.keyAt(i));
+                            stringImageText = stringImageText + " " + textBlock.getValue();
+                        }
+                        textView.setText(stringImageText);
+
+                        /* Delete file: */
+                        photoFile.delete();
+                    }
+
+
+                } else if(resultCode==CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                    Exception error=result.getError();
+                    Toast.makeText(this, ""+error, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(MainActivity.this,storagePermission,STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result= ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermission(){
+
+        boolean result= ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)==(PackageManager.PERMISSION_GRANTED);
+        boolean result1= ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+
+    }
+    private void pickCamera() {
+        ContentValues values= new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE,"NewPic");
+        values.put(MediaStore.Images.Media.DESCRIPTION,"Image To Text");
+        image_uri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        Intent cameraIntent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(cameraIntent,IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void pickGallery() {
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_GALLERY_CODE);
+    }
+
+    public static File getFile(Context context, Uri uri) throws IOException {
+        File directory = new File(context.getFilesDir(), "photos");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+//        File destinationFilename = new File(context.getFilesDir().getPath() + File.separatorChar + queryName(context, uri));
+        File destinationFilename = new File(directory ,"photo.jpg");
+        try (InputStream ins = context.getContentResolver().openInputStream(uri)) {
+            createFileFromStream(ins, destinationFilename);
+        } catch (Exception ex) {
+            Log.e("Save File", ex.getMessage());
+            ex.printStackTrace();
+        }
+        return destinationFilename;
+    }
+    public static void createFileFromStream(InputStream ins, File destination) {
+        try (OutputStream os = new FileOutputStream(destination)) {
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = ins.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+            os.flush();
+        } catch (Exception ex) {
+            Log.e("Save File", ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    private static String queryName(Context context, Uri uri) {
+        Cursor returnCursor =
+                context.getContentResolver().query(uri, null, null, null, null);
+        assert returnCursor != null;
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        returnCursor.moveToFirst();
+        String name = returnCursor.getString(nameIndex);
+        returnCursor.close();
+        return name;
+    }
+
+}
